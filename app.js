699b57dff83d65079ddf78b2c31e62c63096f538
ravipativenu <venugopal.ravipati@gmail.com>
2024-08-16 13:52:57 -0500

    approuter docs updated

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let express = require('express');
+const path = require('path');
 let app = express();
 const { readK8SServices } = require('./lib/k8s');
-const { getDestEnv } = require("./lib/utils/vcap-utils")
+const { getDestEnv, getServiceEnv } = require("./lib/utils/vcap-utils")
 const bootstrap = require('./lib/bootstrap')
+const xsservices = require('./lib/env/xsservices')
+const servicebingindservices = require('./lib/env/serviceBindingService')
 
 let options = {};
 let routerConfig = {};
@@ -14,8 +17,12 @@ app.get('/', function (req, res) {
       "/current-date",
       "/fibo/:n",
       "/k8sservices",
-      "/vcaputils/getdestenv",
-      "/getrouterconfig"
+      "/utils/vcaputils/getdestenv",
+      "/utils/vcaputils/getserviceenv",
+      "/getrouterconfig",
+      "/env/xsenv/loaddefaultservices",
+      "/env/xsenv/filterservices",
+      "/env/servicebindingservice/readfiles"
     ]
   };
   res.send(obj);
@@ -47,13 +54,38 @@ app.get('/k8sservices', function (req, res) {
   res.send(obj);
 });
 
-app.get('/vcaputils/getdestenv', function (req, res) {
+app.get('/utils/vcaputils/getdestenv', function (req, res) {
   let obj = getDestEnv();
+  res.send(obj);
+});
+
+app.get('/utils/vcaputils/getserviceenv', function (req, res) {
+  let obj = getServiceEnv();
   res.send(obj);
 });
 
 app.get('/getrouterconfig', function (req, res) {
   res.send(routerConfig);
+});
+
+app.get('/env/xsenv/loaddefaultservices', function (req, res) {
+  let defaultServices = xsservices.loadDefaultServices(path.join(process.cwd(), 'default-services.json'));
+  res.send(defaultServices);
+});
+
+app.get('/env/xsenv/filterservices', function (req, res) {
+  let defaultServices = xsservices.filterServices({ "name" : "my-xsuaa" } );
+  res.send(defaultServices);
+});
+
+app.get('/env/servicebindingservice/readservicebindingservices', function (req, res) {
+  let serviceBindingRoot = servicebingindservices.readServiceBindingServices();
+  res.send(serviceBindingRoot);
+});
+
+app.get('/env/servicebindingservice/readfiles', function (req, res) {
+  let directoryData = servicebingindservices.readFiles(path.join(process.cwd(), "\\bindings\\xsuaa"));
+  res.send(directoryData);
 });
 
 
